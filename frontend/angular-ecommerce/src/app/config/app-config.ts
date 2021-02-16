f361e2cc18ff2c2ad16de5b597c2df34e77b3c1b
Fabio Abreu <fnmabreu@gmail.com>
2021-02-16 17:28:00 +0000

    Release 3.0 - Change config

--- frontend/angular-ecommerce/src/app/config/app-config.ts
+++ frontend/angular-ecommerce/src/app/config/app-config.ts
@@ -1,9 +1,9 @@
 export default {
 
     oidc: {
-        clientId: '0oa627zjdzMV4xSkD5d6',
-        issuer: 'https://dev-49383836.okta.com/oauth2/default',
-        redirectUri: 'http://localhost4200/login/callback',
+        clientId: '<<UPDATE-WITH-YOUR-APP-CLIENT-ID>>',
+        issuer: 'https://<<UPDATE-WITH-YOUR-DEV-DOMAIN>>/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
